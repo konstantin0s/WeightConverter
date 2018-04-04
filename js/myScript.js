3b54799f2188c98bad991012bac9e895ce968bcd
konstantin0s <constantintofan85@gmail.com>
2018-04-04 22:10:02 +0200

    Add grams,kg and ounces conversion

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,8 @@
-document.getElementById('lbsInput').addEventListener('input', function(e){
-  let pounds = e.target.value;
-  console.log(pounds);
+document.getElementById("output").style.visibility = "hidden";
+document.getElementById("lbsInput").addEventListener("input", function(e) {
+  document.getElementById("output").style.visibility = "visible";
+  let lbs = e.target.value;
+  document.getElementById("gramsOutput").innerHTML = lbs / 0.0022046;
+  document.getElementById("kgOutput").innerHTML = lbs / 2.2046;
+  document.getElementById("ozOutput").innerHTML = lbs * 16;
 });
